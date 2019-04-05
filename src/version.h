2cf31ac15f0cc2df1d1df6e1cdaee8bf2d81f06d
XtendCash <49289946+XtendCash@users.noreply.github.com>
2019-04-05 17:50:29 +0300

    Start of Xtend Protocol

---
+++ src/version.h
@@ -0,0 +1,10 @@
+#pragma once
+
+extern const int XTEND_VERSION_MAJOR;
+extern const int XTEND_VERSION_MINOR;
+extern const int XTEND_VERSION_PATCH;
+
+extern const char* const XTEND_VERSION_TAG;
+extern const char* const XTEND_VERSION;
+extern const char* const XTEND_RELEASE_NAME;
+extern const char* const XTEND_VERSION_FULL;
